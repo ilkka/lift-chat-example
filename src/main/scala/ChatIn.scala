83fbba7a05efe7fcc36b43c98f1f3d4317b00ba2
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2011-09-13 23:20:51 +0300

    ChatIn snippet

---
+++ src/main/scala/ChatIn.scala
@@ -0,0 +1,27 @@
+// vim: filetype=scala
+package code
+package snippet
+
+import net.liftweb._
+import http._
+import js._
+import jsCmds._
+import JE._
+
+import comet.ChatServer
+
+/**
+ * Snippets transform input to output, templates into dynamic content.
+ */
+object ChatIn {
+	/**
+	 * The render method returns a function that transforms NodeSeq ->
+	 * NodeSeq. In this case it transforms a form input by attaching a
+	 * behavior that sends the message to the ChatServer, then returns
+	 * javascript to clear the input.
+	 */
+	def render = SHtml.onSubmit(s => {
+		ChatServer ! s
+		SetValById("chat_in", "")
+	})
+}
